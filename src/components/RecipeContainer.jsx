d27f3f11c8497c4cd9037e9223eb8ed00d3ad091
Aastha Saxena <aasthasaxena100@gmail.com>
2024-06-04 15:33:55 -0400

    Added filter

--- src/components/RecipeContainer.jsx
+++ src/components/RecipeContainer.jsx
@@ -5,17 +5,31 @@ import SubscriptionForm from './SubscriptionForm'
 import Footer from './Footer'
 import LatestRecipeContainer from './LatestRecipeContainer'
 import { RecipesContext } from '../context/recipesContext'
+import FilterRecipes from './FilterRecipes'
+
 
 const RecipeContainer = () => {
 
-  const recipes = useContext(RecipesContext);
+  const { rec, setRec } = useContext(RecipesContext);
+  const [selectedRecipes, setSelectedRecipes] = useState([])
+
+  useEffect(() => {
+    // Update selectedRecipes whenever recipes change
+    setSelectedRecipes(rec);
+  }, [rec]);
+
+  function handleSearch(searchText) {
+    const filteredRecipes = rec.filter((recipe) => recipe.title.toLowerCase().includes(searchText.toLowerCase()))
+    setSelectedRecipes(filteredRecipes)
+  }
 
 
   return (
     <div>
       <Navbar />
       <Subsection image="/cookware.svg" message="All Recipes" />
-      {(recipes.rec).length>0 ? <LatestRecipeContainer heading="Latest Recipes" recipes={recipes.rec} />:<h1 className='text-2xl font-semibold text-center p-10 animate-ping'>Loading...</h1>}
+      <FilterRecipes onSearch={handleSearch} />
+      {(rec).length > 0 ? <LatestRecipeContainer heading="Latest Recipes" recipes={selectedRecipes} /> : <h1 className='text-2xl font-semibold text-center p-10 animate-ping'>Loading...</h1>}
       <SubscriptionForm />
       <Footer />
     </div>
